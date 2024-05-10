da775f49320db1d467b2e4373c38d492fb2303e2
Kennedy <121739110+kbrieva@users.noreply.github.com>
2024-05-10 21:22:14 +0800

    add new eventloader during loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,29 @@
     }
   }
 
+/** Preloader **/
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  }
+})
+
+/** end of preloader **/
+
   /**
    * Easy event listener function
    */
